baa2ecebb41020a93fd3b3fec2a1a42f36b0de53
Emiliano Bruni <info@ebruni.it>
2026-01-07 21:18:16 +0100

    fix: remove unused webpack configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,6 @@
 const nextConfig = {
     output: "export",
     distDir: "notepadps-bz",
-    webpack: (config, options) => {
-        // per verificare che watch guardi le directory giuste
-        // strace -fe inotify_add_watch npm run dev
-        // remove node_modules
-        config.watchOptions.ignored.push("**/node_modules");
-        // don't know why but it try to watch /ws/folder_name/ws/foldername too
-        config.watchOptions.ignored.push("**/ws");
-        // don't know but it watch .. too so don't create app in /first_level dir
-        // /app is bad
-        return config;
-    },
     typescript: {
         ignoreBuildErrors: true,
     },
